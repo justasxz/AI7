41499ac393c980af0b92a3e05d7d017fdb98521a
Justas Kvederis <justas@computech.ie>
2025-09-02 19:14:16 +0300

    .

--- knn_with_test.py
+++ knn_with_test.py
@@ -33,47 +33,92 @@ y = df['Survived']
 
 
 df_test = pd.read_csv('data/titanic_test.csv')
-X_test_final = preprocess_data(df_test)
-
-scaler = StandardScaler()
-# exclude non-numeric and binary columns from scaling
-columns_to_scale = ['Age', 'SibSp', 'Parch', 'Fare', 'Pclass']
-
-X[columns_to_scale] = scaler.fit_transform(X[columns_to_scale])
-X_test_final[columns_to_scale] = scaler.transform(X_test_final[columns_to_scale])
-# param_grid = {'n_neighbors': list(range(1, 31))}
-k_values = list(range(2, 31))
-mean_scores = []
-for k in k_values:
-    knn = KNeighborsClassifier(n_neighbors=k)
-    scores = cross_val_score(knn, X, y, cv=5, scoring='accuracy')
-    mean_scores.append(scores.mean())
 import seaborn as sns
 import matplotlib.pyplot as plt
-sns.lineplot(x=k_values, y=mean_scores)
-plt.xlabel('Number of Neighbors K')
-plt.ylabel('Cross-Validated Accuracy')
-plt.title('KNN Hyperparameter Tuning')
-# plt.show()    
-best_k = k_values[np.argmax(mean_scores)]
-print(f'Best K: {best_k}')
+# sns.countplot(data=df, x='Survived')
+# plt.show()
+# sns.heatmap(df.corr(), annot=True)
+# plt.show()
 
-knn = KNeighborsClassifier(n_neighbors=best_k)
-knn.fit(X, y)
+# sns.histplot(df['Age'].dropna(), kde=True, bins=10)
+# plt.show()
 
-# let's make test columns the same as train
-missing_cols_in_train = set(X_test_final.columns) - set(X.columns)
-print(missing_cols_in_train)
-missing_cols_in_test = set(X.columns) - set(X_test_final.columns)
-print(missing_cols_in_test)
-# drop missing columns in test
-X_test_final.drop(columns=missing_cols_in_train, inplace=True)
+# columns_to_plot = df.select_dtypes(include=[np.number]).columns.tolist()
 
-y_pred = knn.predict(X_test_final)
+# fig, axes = plt.subplots(len(columns_to_plot), 2, figsize=(8, 5 * len(columns_to_plot)))
 
-# let's create a submission file
-submission = pd.DataFrame({
-    'PassengerId': df_test['PassengerId'],
-    'Survived': y_pred
-})
-submission.to_csv('data/titanic_submission.csv', index=False)
+# for i, col in enumerate(columns_to_plot):
+#     sns.histplot(df[col].dropna(), kde=True, bins=10, ax=axes[i, 0])
+#     axes[i, 0].set_title(f'Histogram of {col}')
+    
+#     sns.boxplot(x=df[col], ax=axes[i, 1])
+#     axes[i, 1].set_title(f'Boxplot of {col}')
+# plt.tight_layout()
+# plt.show()
+
+# fig, axes = plt.subplots(2, 2, figsize=(12, 10))
+
+# sns.histplot(df['Age'].dropna(), kde=True, bins=10, ax=axes[0,0])
+# sns.histplot(df['Age'].dropna(), kde=True, bins=10)
+# sns.boxplot(x=df['Age'], ax=axes[0,1])
+# plt.tight_layout()
+# plt.show()
+
+# for col in columns_to_plot:
+#     sns.histplot(df[col].dropna(), kde=True, bins=10)
+#     plt.title(f'Histogram of {col}')
+#     plt.show()
+# sns.histplot(df['Fare'].dropna(), kde=True, bins=5)
+# plt.show()
+
+
+# X_test_final = preprocess_data(df_test)
+
+# scaler = StandardScaler()
+# # exclude non-numeric and binary columns from scaling
+# columns_to_scale = ['Age', 'SibSp', 'Parch', 'Fare', 'Pclass']
+
+# X[columns_to_scale] = scaler.fit_transform(X[columns_to_scale])
+# X_test_final[columns_to_scale] = scaler.transform(X_test_final[columns_to_scale])
+# # param_grid = {'n_neighbors': list(range(1, 31))}
+# k_values = list(range(2, 31))
+# mean_scores = []
+# for k in k_values:
+#     knn = KNeighborsClassifier(n_neighbors=k)
+#     scores = cross_val_score(knn, X, y, cv=5, scoring='accuracy')
+#     mean_scores.append(scores.mean())
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+# sns.lineplot(x=k_values, y=mean_scores)
+# plt.xlabel('Number of Neighbors K')
+# plt.ylabel('Cross-Validated Accuracy')
+# plt.title('KNN Hyperparameter Tuning')
+# # plt.show()    
+# best_k = k_values[np.argmax(mean_scores)]
+# print(f'Best K: {best_k}')
+
+# knn = KNeighborsClassifier(n_neighbors=best_k)
+# knn.fit(X, y)
+
+# # let's make test columns the same as train
+# missing_cols_in_train = set(X_test_final.columns) - set(X.columns)
+# print(missing_cols_in_train)
+# missing_cols_in_test = set(X.columns) - set(X_test_final.columns)
+# print(missing_cols_in_test)
+# # drop missing columns in test
+# X_test_final.drop(columns=missing_cols_in_train, inplace=True)
+
+# y_pred = knn.predict(X_test_final)
+
+# # let's create a submission file
+# submission = pd.DataFrame({
+#     'PassengerId': df_test['PassengerId'],
+#     'Survived': y_pred
+# })
+# submission.to_csv('data/titanic_submission.csv', index=False)
+
+# df['column'].fillna(df['column'].median(), inplace=True)
+# df.fillna({'Column': df['Column'].median()}, inplace=True)
+# df[col].method(value, inplace=True)
+# df.method({col: value}, inplace=True)
+# df[col] = df[col].method(value)
